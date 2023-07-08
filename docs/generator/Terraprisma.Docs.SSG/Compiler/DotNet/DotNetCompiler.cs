31fbddf513d9aad146980e822d016922f518d1aa
Tomat <xxlennygamerxx@gmail.com>
2023-07-08 00:53:09 -0700

    Use MSBuild to build .NET projects, detect output assembly and summary paths.

--- docs/generator/Terraprisma.Docs.SSG/Compiler/DotNet/DotNetCompiler.cs
+++ docs/generator/Terraprisma.Docs.SSG/Compiler/DotNet/DotNetCompiler.cs
@@ -1,4 +1,9 @@
-﻿using Terraprisma.Docs.SSG.Configuration;
+﻿using Microsoft.Build.Construction;
+using Microsoft.Build.Definition;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Execution;
+using Microsoft.Build.Locator;
+using Terraprisma.Docs.SSG.Configuration;
 
 namespace Terraprisma.Docs.SSG.Compiler.DotNet;
 
@@ -11,6 +16,41 @@ public sealed class DotNetCompiler : ICompiler {
         if (!File.Exists(ns.Input))
             throw new FileNotFoundException($"The file {ns.Input} does not exist.");
 
+        if (!MSBuildLocator.IsRegistered) {
+            var instance = MSBuildLocator.QueryVisualStudioInstances().First();
+            MSBuildLocator.RegisterInstance(instance);
+        }
+
+        var (assemblyFilePath, summaryFilePath) = CompileMsBuildProject(ns.Input);
+        return GenerateDocumentationFromAssembly(assemblyFilePath, summaryFilePath);
+    }
+
+    private (string assemblyFilePath, string? summaryFilePath) CompileMsBuildProject(string projectPath) {
+        var projectCollection = new ProjectCollection();
+        var project = new Project(projectPath, null, null, projectCollection);
+
+        var outputDir = project.GetPropertyValue("OutputPath");
+        var outputFileName = project.GetPropertyValue("AssemblyName") + ".dll";
+        var summaryFileName = project.GetPropertyValue("DocumentationFile");
+
+        var outputPath = Path.Combine(Path.GetDirectoryName(projectPath)!, outputDir, outputFileName);
+        var summaryPath = Path.Combine(Path.GetDirectoryName(projectPath)!, outputDir, summaryFileName);
+
+        if (!project.Build())
+            throw new Exception("Failed to build project.");
+
+        if (!File.Exists(outputPath))
+            throw new FileNotFoundException($"The output file '{outputPath}' does not exist.");
+
+        if (string.IsNullOrEmpty(summaryFileName) || !File.Exists(summaryPath)) {
+            Console.WriteLine($"    Warning: The summary file '{summaryPath}' does not exist.");
+            summaryPath = null;
+        }
+
+        return (outputPath, summaryPath);
+    }
+
+    private Dictionary<string, string> GenerateDocumentationFromAssembly(string assemblyFilePath, string? summaryFilePath) {
         return new Dictionary<string, string>();
     }
 }
