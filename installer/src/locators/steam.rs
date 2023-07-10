8ac1f7edc08b33a7a931ebbe42a930328bfeefc2
Tomat <xxlennygamerxx@gmail.com>
2023-07-09 17:42:36 -0700

    Reinstance Steam locator.

--- installer/src/locators/steam.rs
+++ installer/src/locators/steam.rs
@@ -6,10 +6,10 @@ const TERARRIA_APPID: &u32 = &105600;
 
 // TODO rewrite this to not use steam-locator. It uses an outdated crate and
 // could check some more reg keys on windows.
-pub fn check_steam() -> PathBuf {
+pub fn check_steam() -> Option<PathBuf> {
     let mut steam = SteamDir::locate().unwrap();
     match steam.app(TERARRIA_APPID) {
-        None => panic!("Terraria is not installed"),
-        Some(p) => p.path.to_owned(),
+        None => None,
+        Some(p) => Some(p.path.to_owned()),
     }
 }
