b654c9ed205f8b50f28948da9ffdb62acabb8324
Tomat <xxlennygamerxx@gmail.com>
2023-09-30 21:00:04 -0700

    a

--- nightglow/Terraprisma.Nightglow/Program.cs
+++ nightglow/Terraprisma.Nightglow/Program.cs
@@ -1,9 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using CliFx;
 
 namespace Terraprisma.Nightglow;
 
 internal static class Program {
+    [STAThread]
     internal static async Task<int> Main(string[] args) {
         return await new CliApplicationBuilder().AddCommandsFromThisAssembly().Build().RunAsync(args);
     }
