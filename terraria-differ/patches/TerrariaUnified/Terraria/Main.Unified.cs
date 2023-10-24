b66744cbd230940249f8edf3e1019bce03b41e18
Tomat <xxlennygamerxx@gmail.com>
2023-10-23 21:56:11 -0700

    Add obnoxious branding.

---
+++ terraria-differ/patches/TerrariaUnified/Terraria/Main.Unified.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Terraria.DataStructures;
+
+namespace Terraria;
+
+partial class Main
+{
+	internal static string unifiedVersion = "0.1.0";
+
+	internal static List<TitleLinkButton> unifiedLinks = new List<TitleLinkButton>();
+}
